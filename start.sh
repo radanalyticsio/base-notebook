14694964f06d825df855ed18c68655c42e88d9ce
William Benton <willb@redhat.com>
2017-04-12 12:39:59 -0500

    added option to set notebook password in environment

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [[ "x$JUPYTER_NOTEBOOK_PASSWORD" != "x" ]]; then
+    HASH=$(python -c "from notebook.auth import passwd; print(passwd('$JUPYTER_NOTEBOOK_PASSWORD'))")
+    echo "c.NotebookApp.password = u'$HASH'" >> /home/$NB_USER/.jupyter/jupyter_notebook_config.py
+fi
+
 export PYTHONPATH=$SPARK_HOME/python:$(echo $SPARK_HOME/python/lib/py4j-*-src.zip)
 exec jupyter notebook
